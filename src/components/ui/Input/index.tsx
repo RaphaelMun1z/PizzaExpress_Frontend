852d79cb0617ed2d9c6d97a5d767ae2f567f6b28
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-05-19 18:07:15 -0300

    Att

---
+++ src/components/ui/Input/index.tsx
@@ -0,0 +1,23 @@
+import { InputHTMLAttributes, TextareaHTMLAttributes } from 'react'
+import styles from './Styles.module.scss'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> { }
+
+interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> { }
+
+const Input = ({ ...rest }: InputProps) => {
+    return (
+        <input className={styles.input} {...rest} />
+    )
+}
+
+const TextArea = ({ ...rest }: TextAreaProps) => {
+    return (
+        <textarea className={styles.input} {...rest}></textarea>
+    )
+}
+
+export default {
+    Input,
+    TextArea
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
 import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss"
+
+import logoImg from "../../public/logo.png"
+
+import Input from "../components/ui/Input";
 
 export default function Home() {
   return (
@@ -6,7 +12,15 @@ export default function Home() {
       <Head>
         <title>PizzaExpress - Faça seu login</title>
       </Head>
-      <h1>NextJS</h1>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo Pizza Express" />
+        <div className={styles.login}>
+          <form>
+            <Input placeholder="Digite seu e-mail" type="text" />
+            <Input placeholder="Digite sua senha" type="password" />
+          </form>
+        </div>
+      </div>
     </>
   );
 }
