0da4d9ad405841031c1da49bb70e53f495206809
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-05-23 18:38:40 -0300

    Att

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,74 @@
+import styles from "./Styles.module.scss"
+import { useState } from "react"
 import { canSSRAuth } from "../../utils/canSSRAuth"
 import Head from "next/head"
 
 import Header from "../../components/Header"
+import { FiRefreshCcw } from "react-icons/fi"
 
-const Dashboard = () => {
+import { setupAPIClient } from "../../services/api"
+
+import ModalOrder from "../../components/ModalOrder"
+
+import Modal from "react-modal"
+
+type OrderProps = {
+  id: string
+  table: string | number
+  status: boolean
+  draft: boolean
+  name: string | null
+}
+
+interface HomeProps {
+  orders: OrderProps[]
+}
+
+export type OrderItemProps = {
+  id: string
+  amount: number
+  order_id: string
+  product_id: string
+  product: {
+    id: string
+    name: string
+    description: string
+    price: string
+    banner: string
+  }
+  order: {
+    id: string
+    table: string | number
+    status: boolean
+    name: string | null
+  }
+}
+
+const Dashboard = ({ orders }: HomeProps) => {
+  const [orderList, setOrderList] = useState(orders || [])
+
+  const [modalItem, setModalItem] = useState<OrderItemProps[]>()
+  const [modalVisible, setModalVisible] = useState(false)
+
+  function handleCloseModal() {
+    setModalVisible(false)
+  }
+
+  async function handleOpenModalView(id: string) {
+    const apiClient = setupAPIClient()
+
+    const response = await apiClient.get("/order/detail", {
+      params: {
+        order_id: id
+      }
+    })
+
+    setModalItem(response.data)
+    setModalVisible(true)
+  }
+
+  Modal.setAppElement("#__next")
+
   return (
     <>
       <Head>
@@ -11,7 +76,34 @@ const Dashboard = () => {
       </Head>
       <div>
         <Header />
-        <h1>Painel</h1>
+        <main className={styles.container}>
+          <div className={styles.containerHeader}>
+            <h1>Últimos pedidos</h1>
+            <button>
+              <FiRefreshCcw size={25} color="#2fffa3" />
+            </button>
+          </div>
+
+          <article className={styles.listOrders}>
+
+            {orderList.map((item) => (
+              <section key={item.id} className={styles.orderItem}>
+                <button onClick={() => handleOpenModalView(item.id)}>
+                  <div className={styles.tag}></div>
+                  <span>Mesa {item.table}</span>
+                </button>
+              </section>
+            ))}
+
+          </article>
+        </main>
+        {modalVisible && (
+          <ModalOrder
+            isOpen={modalVisible}
+            onRequestClose={handleCloseModal}
+            order={modalItem}
+          />
+        )}
       </div>
     </>
   )
@@ -20,9 +112,13 @@ const Dashboard = () => {
 export default Dashboard
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx)
+
+  const response = await apiClient.get("/order")
+
   return {
     props: {
-
+      orders: response.data
     }
   }
 })
